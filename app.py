f325bc1102cd3b5ae0d05a4329b364b994094693
ch6gm556p2-web <ch6gm556p2@privaterelay.appleid.com>
2026-06-12 15:18:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -112,7 +112,6 @@ if st.button("Analyze"):
             row2col1.metric("Investment Score", f"{investment_score}/100")
             row2col2.metric("Est. 20-Day Value", f"${estimated_20d_price:,.2f}")
             row2col3.markdown("**Likely Range**")
-            row2col3.markdown("**Likely Range**")
             row2col3.markdown(
                 f"<h2>${lower_price:,.0f} - ${upper_price:,.0f}</h2>",
                 unsafe_allow_html=True
