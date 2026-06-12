08549f20e00b51f65fb83b386d953f6b3873f080
ch6gm556p2-web <ch6gm556p2@privaterelay.appleid.com>
2026-06-12 14:01:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ if st.button("Analyze"):
 
             col1, col2, col3 = st.columns(3)
 
-            col1.metric("Current Price", f"${current_price:,.2f}")
+            col1.metric("Current Price", f"${float(current_price):,.2f}")
             col2.metric("Model Accuracy", f"{accuracy:.1%}")
             col3.metric("20-Day Probability", f"{probability:.1%}")
     
